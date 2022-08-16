cf3bc23a7aba2c874f7cdfbe8cb83dd3dea1ee52
Jahernandezp <110497924+Jahernandezp@users.noreply.github.com>
2022-08-15 19:08:30 -0600

    Modify clientes.json

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -5,22 +5,6 @@ export const CLIENTES: Cliente[] = [
     ,{	id	:		2	,	nombre	:	'	Emmanuel	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Emmanuel.Hernandez@gmail.com	'}	
     ,{	id	:		3	,	nombre	:	'	Irma	'	,	apellido	:	'	Canedo	'	,	fecha	:		'01-05-22'		,	email	:	'	Irma.Canedo@gmail.com	'}	
     ,{	id	:		4	,	nombre	:	'	David	'	,	apellido	:	'	Tirado	'	,	fecha	:		'01-05-22'		,	email	:	'	David.Tirado@gmail.com	'}
-    ,{	id	:		1	,	nombre	:	'	Jesus A	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Jesus A.Hernandez@gmail.com	'}
-    ,{	id	:		2	,	nombre	:	'	Emmanuel	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Emmanuel.Hernandez@gmail.com	'}	
-    ,{	id	:		3	,	nombre	:	'	Irma	'	,	apellido	:	'	Canedo	'	,	fecha	:		'01-05-22'		,	email	:	'	Irma.Canedo@gmail.com	'}	
-    ,{	id	:		4	,	nombre	:	'	David	'	,	apellido	:	'	Tirado	'	,	fecha	:		'01-05-22'		,	email	:	'	David.Tirado@gmail.com	'}
-    ,{	id	:		1	,	nombre	:	'	Jesus A	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Jesus A.Hernandez@gmail.com	'}
-    ,{	id	:		2	,	nombre	:	'	Emmanuel	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Emmanuel.Hernandez@gmail.com	'}	
-    ,{	id	:		3	,	nombre	:	'	Irma	'	,	apellido	:	'	Canedo	'	,	fecha	:		'01-05-22'		,	email	:	'	Irma.Canedo@gmail.com	'}	
-    ,{	id	:		4	,	nombre	:	'	David	'	,	apellido	:	'	Tirado	'	,	fecha	:		'01-05-22'		,	email	:	'	David.Tirado@gmail.com	'}
-    ,{	id	:		1	,	nombre	:	'	Jesus A	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Jesus A.Hernandez@gmail.com	'}
-    ,{	id	:		2	,	nombre	:	'	Emmanuel	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Emmanuel.Hernandez@gmail.com	'}	
-    ,{	id	:		3	,	nombre	:	'	Irma	'	,	apellido	:	'	Canedo	'	,	fecha	:		'01-05-22'		,	email	:	'	Irma.Canedo@gmail.com	'}	
-    ,{	id	:		4	,	nombre	:	'	David	'	,	apellido	:	'	Tirado	'	,	fecha	:		'01-05-22'		,	email	:	'	David.Tirado@gmail.com	'}
-    ,{	id	:		1	,	nombre	:	'	Jesus A	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Jesus A.Hernandez@gmail.com	'}
-    ,{	id	:		2	,	nombre	:	'	Emmanuel	'	,	apellido	:	'	Hernandez	'	,	fecha	:		'01-05-22'		,	email	:	'	Emmanuel.Hernandez@gmail.com	'}	
-    ,{	id	:		3	,	nombre	:	'	Irma	'	,	apellido	:	'	Canedo	'	,	fecha	:		'01-05-22'		,	email	:	'	Irma.Canedo@gmail.com	'}	
-    ,{	id	:		4	,	nombre	:	'	David	'	,	apellido	:	'	Tirado	'	,	fecha	:		'01-05-22'		,	email	:	'	David.Tirado@gmail.com	'}
-
+    
 
   ];
